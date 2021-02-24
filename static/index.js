e253768e20d0a0f109138fd0d0166e9d1f1f0e14
Nneka Tielman <khalie_nne@yahoo.com>
2021-02-25 00:14:31 +0100

    my profile styling done

--- static/index.js
+++ static/index.js
@@ -1,1 +1,11 @@
-//mobile menu
+// sign in modal
+const signInButton = document.querySelector('#sign_in');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+signInButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
